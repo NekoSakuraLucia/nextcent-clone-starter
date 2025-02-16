abe1032f2e60de7679c92cf58c5021f305de0849
NekoSakuraLucia <mokurolucia@gmail.com>
2025-02-16 22:43:13 +0700

    เพิ่ม className ให้กับแต่ละ element ของส่วนคอมโพเนนต์ `Header.jsx`

--- web-nextcent/src/components/Header.jsx
+++ web-nextcent/src/components/Header.jsx
@@ -2,16 +2,16 @@ import HeaderImg from '../assets/img/header-img.png'
 
 const Header = () => {
     return (
-        <div>
-            <div>
-                <div>
-                    <h1>
+        <div className='flex items-center bg-[#F5F7FA] h-auto md:h-[37.5rem]'>
+            <div className='container mx-auto max-w-[1320px] p-10 md:p-0 flex flex-col md:flex-row md:justify-between md:items-center'>
+                <div className=''>
+                    <h1 className='text-[4rem] leading-[1] font-semibold text-[#4D4D4D]'>
                         Lessons and insights
                         <br />
-                        <span>from 8 years</span>
+                        <span className='text-[#4CAF4F]'>from 8 years</span>
                     </h1>
-                    <p>Where to grow your business as a photographer: site or social media?</p>
-                    <a href="#">Register</a>
+                    <p className='text-[#717171] mt-5'>Where to grow your business as a photographer: site or social media?</p>
+                    <a href="#" className='mt-10 inline-flex justify-center items-center py-3 px-8 bg-[#4CAF4F] rounded-md text-white'>Register</a>
                 </div>
                 <div>
                     <img src={HeaderImg} alt="HeaderImg" />
