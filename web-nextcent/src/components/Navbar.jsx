dbccc5605a90a06e2b18d92474605dbae48522be
NekoSakuraLucia <mokurolucia@gmail.com>
2025-02-16 18:57:40 +0700

    เพิ่มการตกแต่งเริ่มต้นแล้ว เหลือแค่ state action

--- web-nextcent/src/components/Navbar.jsx
+++ web-nextcent/src/components/Navbar.jsx
@@ -1,42 +1,42 @@
-import Logo from '../assets/img/logo-black.png';
+import Logo from '../assets/img/logo-black.png'
 
 const Navbar = () => {
     return (
-        <nav>
-            <div>
+        <nav className="bg-[#F5F7FA]">
+            <div className='relative container mx-auto max-w-[1320px] p-10 h-auto flex flex-col md:flex-row md:items-center md:justify-between md:h-[80px]'>
                 <div>
                     <a href="#">
                         <img src={Logo} alt="Logo" />
                     </a>
                 </div>
 
-                <ul>
-                    <li>
+                <ul className='flex flex-col md:flex-row my-5'>
+                    <li className='my-2 md:mx-4'>
                         <a href="#">Home</a>
                     </li>
-                    <li>
+                    <li className='my-2 md:mx-4'>
                         <a href="#">Service</a>
                     </li>
-                    <li>
+                    <li className='my-2 md:mx-4'>
                         <a href="#">Feature</a>
                     </li>
-                    <li>
+                    <li className='my-2 md:mx-4'>
                         <a href="#">Product</a>
                     </li>
-                    <li>
+                    <li className='my-2 md:mx-4'>
                         <a href="#">Testimonial</a>
                     </li>
-                    <li>
+                    <li className='my-2 md:mx-4'>
                         <a href="#">FAQ</a>
                     </li>
                 </ul>
 
-                <ul>
-                    <li>
-                        <a href="#">Login</a>
+                <ul className='flex flex-col md:flex-row my-5'>
+                    <li className='my-2 md:mx-4'>
+                        <a href="#" className='inline-flex justify-center items-center py-2 px-4 text-[#4CAF4F]'>Login</a>
                     </li>
-                    <li>
-                        <a href="#">Sign up</a>
+                    <li className='my-2 md:mx-4'>
+                        <a href="#" className='inline-flex justify-center items-center py-2 px-4 bg-[#4CAF4F] rounded-md text-white'>Sign up</a>
                     </li>
                 </ul>
             </div>
