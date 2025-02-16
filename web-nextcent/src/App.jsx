4b33ac1899507d11476575507b33675c396216a5
NekoSakuraLucia <mokurolucia@gmail.com>
2025-02-16 20:20:34 +0700

    ย้าย Navbar จากเดิม `main.jsx` มาไฟล์ `App.jsx` และ นำ Header มาใช้

--- web-nextcent/src/App.jsx
+++ web-nextcent/src/App.jsx
@@ -1,5 +1,13 @@
+import Navbar from './components/Navbar'
+import Header from './components/Header';
+
 function App() {
-  return <div className="App">สวัสดีโลก</div>
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  )
 }
 
 export default App;
