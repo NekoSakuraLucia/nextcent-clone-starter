4f57d642f86689478ec244b6511dcffa8ebd8289
NekoSakuraLucia <mokurolucia@gmail.com>
2025-02-16 22:30:46 +0700

    เพิ่มส่วนประกอบ และ โครงสร้างของ element (ยังไม่ได้ตกแต่ง)

--- web-nextcent/src/components/Header.jsx
+++ web-nextcent/src/components/Header.jsx
@@ -1,5 +1,24 @@
+import HeaderImg from '../assets/img/header-img.png'
+
 const Header = () => {
-    <div className="Header">Header</div>
+    return (
+        <div>
+            <div>
+                <div>
+                    <h1>
+                        Lessons and insights
+                        <br />
+                        <span>from 8 years</span>
+                    </h1>
+                    <p>Where to grow your business as a photographer: site or social media?</p>
+                    <a href="#">Register</a>
+                </div>
+                <div>
+                    <img src={HeaderImg} alt="HeaderImg" />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header;
